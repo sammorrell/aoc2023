b197922813416becfed9b8a457f8d822451228b6
Sam Morrell <samuelmorrell@gmail.com>
2023-12-04 23:51:29 +0000

    [Day 4] Solution for part 1.
    The parsing was a little fiddly. Otherwise it was none too bad.

---
+++ src/days/day4.rs
@@ -0,0 +1,73 @@
+use aoctk::io::read_string_col;
+use regex::Regex;
+use std::path::Path;
+
+#[derive(Debug)]
+pub struct Scratchcard {
+    pub game_id: u32,
+    pub winning_numbers: Vec<u32>,
+    pub numbers: Vec<u32>,
+}
+
+pub fn parse_cards_from_file(path: &Path) -> Vec<Scratchcard> {
+    let split_re = Regex::new("[ ]+").unwrap();
+    let card_re = Regex::new("Card[ ]+").unwrap();
+    let card_lines = read_string_col(path).expect("Unable to read card file. ");
+    card_lines
+        .iter()
+        .map(|line| {
+            let line_segs: Vec<&str> = line.split(": ").collect();
+            let game_id = card_re
+                .replace(line_segs.first().unwrap(), "")
+                .parse::<u32>()
+                .unwrap();
+            let number_strings: Vec<&str> = line_segs.last().unwrap().split(" | ").collect();
+            let winning_numbers = split_re
+                .split(
+                    number_strings
+                        .first()
+                        .expect("No winning numbers found. ")
+                        .trim(),
+                )
+                .map(|str| str.trim().parse().unwrap())
+                .collect();
+            let numbers = split_re
+                .split(number_strings.last().expect("No numbers found. ").trim())
+                .map(|str| str.trim().parse().unwrap())
+                .collect();
+            Scratchcard {
+                game_id,
+                winning_numbers,
+                numbers,
+            }
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_cards_from_file;
+    use std::{collections::HashSet, path::Path};
+
+    #[test]
+    fn day4_part1() {
+        let cards = parse_cards_from_file(Path::new("data/day4/data.txt"));
+
+        let scores: Vec<u32> = cards
+            .iter()
+            .map(|card| {
+                let winning_nums: HashSet<u32> =
+                    HashSet::from_iter(card.winning_numbers.iter().cloned());
+                let played_nums: HashSet<u32> = HashSet::from_iter(card.numbers.iter().cloned());
+                let n_winning = winning_nums.intersection(&played_nums).count();
+                if n_winning > 0 {
+                    (0..n_winning - 1).fold(1, |accum, _| accum * 2)
+                } else {
+                    0
+                }
+            })
+            .collect();
+
+        assert_eq!(scores.iter().sum::<u32>(), 21158);
+    }
+}
