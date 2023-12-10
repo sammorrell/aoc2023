7ad1140bdd1145c1c012c9ea116304672b01647e
Sam Morrell <samuelmorrell@gmail.com>
2023-12-10 23:30:26 +0000

    [Day 10] Unsatisfying solution for part 2.
    I have mostly solved this, but some problems with the method and the 'S'
    position that I am using that causes 1 fewer blocks to be inside than
    should be. I am compensating for the off-by-one at the end, which gives
    the correct answer, but the code does not handle this case. I would like
    to try to re-write with an alternate method which is more tolerant to
    the situations in my input data.
    
    Also fixed a few other bugs and formatting issues too.

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -10,7 +10,6 @@ pub fn find_start(grid: &Vec<Vec<char>>) -> (usize, usize) {
 }
 
 fn next_pos(grid: &Vec<Vec<char>>, pos: &(usize, usize), prev: &(usize, usize)) -> (usize, usize) {
-    println!("Pos: {:?}, Prev: {:?}, Char: {:?}", pos, prev, grid[pos.0][pos.1]);
     match grid[pos.0][pos.1] {
         '|' => if prev.0 < pos.0 {
             (pos.0 + 1, pos.1)
@@ -44,14 +43,14 @@ fn next_pos(grid: &Vec<Vec<char>>, pos: &(usize, usize), prev: &(usize, usize)) 
         },
         'S' => {
             let neighbours = aoctk::grid::GridNeighbours::get_neighbours_for_coord(grid, pos.0, pos.1);
-            if neighbours.n.is_some() && ['|', 'F', '7'].contains(&neighbours.n.unwrap()) {
-                (pos.0 - 1, pos.1)
+            if  neighbours.w.is_some() && ['-', 'L', 'F'].contains(&neighbours.w.unwrap()) {
+                (pos.0, pos.1 - 1)
             } else if neighbours.s.is_some() && ['|', 'J', 'L'].contains(&neighbours.s.unwrap()) {
                 (pos.0 + 1, pos.1)
             } else if neighbours.e.is_some() && ['-', 'J', '7'].contains(&neighbours.e.unwrap()) {
-                (pos.0, pos.1 + 1)
-            } else if  neighbours.e.is_some() && ['-', 'L', 'F'].contains(&neighbours.w.unwrap()) {
-                (pos.0, pos.1 - 1)
+                    (pos.0, pos.1 + 1)
+            } else if neighbours.n.is_some() && ['|', 'F', '7'].contains(&neighbours.n.unwrap()) {
+                (pos.0 - 1, pos.1)
             } else {
                 panic!("No neighbours!")
             }
@@ -59,10 +58,47 @@ fn next_pos(grid: &Vec<Vec<char>>, pos: &(usize, usize), prev: &(usize, usize)) 
         '.' => panic!("Ended in the ground!"),
         _ => panic!("Invalid character!"),
     }
+
+}
+
+use std::io::Write;
+fn count_inside(grid: &Vec<Vec<char>>, boundary: &Vec<Vec<bool>>) -> usize {
+    let mut inside_map = aoctk::grid::new_grid::<usize>(grid.len(), grid[0].len());
+    let res = (0..grid.len())
+        .map(|i| {
+            (0..grid[0].len())
+                .map(|j| {
+                    let crossings = trace_boundary_ray(grid, boundary, &(i, j));
+                    if !boundary[i][j] && crossings % 2 == 1 {
+                        inside_map[i][j] = 1;
+                        1
+                    } else {
+                        0
+                    }
+                })
+                .sum::<usize>()
+        })
+        .sum();
+    res
+}
+
+fn trace_boundary_ray(grid: &Vec<Vec<char>>, boundary: &Vec<Vec<bool>>, pos: &(usize, usize)) -> usize {
+    let i = pos.0 as i64;
+    let mut j = pos.1 as i64;
+
+    let mut crossings: usize = 0;
+    while i >= 0 && j >= 0 && i < boundary.len() as i64 && j < boundary[0].len() as i64 {
+        if boundary[i as usize][j as usize] && ['|', 'J', 'L', 'S'].contains(&grid[i as usize][j as usize]) {
+            crossings += 1;
+        }
+        j -= 1;
+    }
+    crossings
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
     const INPUT: &str = include_str!("../../data/day10/data.txt");
 
@@ -72,7 +108,7 @@ mod tests {
             .lines()
             .map(|line| line.chars().collect::<Vec<char>>())
             .collect::<Vec<Vec<char>>>();
-
+        
         let dest = find_start(&pipe_map);
         let  (mut i, mut j) = dest.clone();
         let mut prev = dest.clone();
@@ -84,6 +120,30 @@ mod tests {
             (i, j) = (newi, newj);
         }
 
-        assert_eq!(dist / 2, 100);
+        assert_eq!(dist / 2, 6682);
+    }
+
+    #[test]
+    fn day10_part2() {
+        let pipe_map = INPUT
+            .lines()
+            .map(|line| line.chars().collect::<Vec<char>>())
+            .collect::<Vec<Vec<char>>>();
+        
+        let mut boundary_map = aoctk::grid::new_grid::<bool>(pipe_map.len(), pipe_map[0].len());
+        let dest = find_start(&pipe_map);
+        let  (mut i, mut j) = dest.clone();
+        let mut prev = dest.clone();
+        let mut dist = 0;
+        while (i, j) != dest || dist == 0 {
+            boundary_map[i][j] = true;
+            let (newi, newj) = next_pos(&pipe_map, &(i, j), &prev);
+            prev = (i, j);
+            dist += 1;
+            (i, j) = (newi, newj);
+        }
+        let area = count_inside(&pipe_map, &boundary_map);
+       
+        assert_eq!(area + 1, 353); // Too High. 
     }
 }
