06d95dabeb138bfe2c0a857041a9564db1d6d563
Sam Morrell <samuelmorrell@gmail.com>
2023-12-12 22:43:57 +0000

    [Day 12] Solution for part 2.
    After studying the reddit, and finding the solution of hyper-neutrino for guidance (https://www.youtube.com/watch?v=g3Ms5e7Jdqo). I have also implemented memoisation on the core function for this to improve performance.

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,5 +1,6 @@
 use itertools::Itertools;
 use regex::Regex;
+use memoize::memoize;
 
 const BROKEN_SEPARATOR: &str = "[.]+";
 
@@ -30,7 +31,10 @@ pub fn is_valid_row(row: &str, broken_pattern: &Vec<usize>) -> bool {
         .all(|(group, broken)| group.len() == *broken)
 }
 
-fn count_valid_combinations(row: &str, broken_pattern: &Vec<usize>) -> usize {
+/// This is the brute-force approach that I originally solves part 1 with.
+/// This would not scale to part 2. 
+#[memoize]
+fn count_valid_combinations(row: String, broken_pattern: Vec<usize>) -> usize {
     let unknown_posns = row.match_indices("?").collect_vec();
     if unknown_posns.len() > 0 {
         let pos = unknown_posns.first().unwrap().0;
@@ -39,9 +43,9 @@ fn count_valid_combinations(row: &str, broken_pattern: &Vec<usize>) -> usize {
         let mut broken_test = row.to_string();
         broken_test.replace_range(pos..pos+1, "#");
 
-        count_valid_combinations(&working_test, broken_pattern) + count_valid_combinations(&broken_test, broken_pattern)
+        count_valid_combinations(working_test, broken_pattern.clone()) + count_valid_combinations(broken_test, broken_pattern.clone())
     } else {
-        if is_valid_row(row, broken_pattern) {
+        if is_valid_row(&row, &broken_pattern) {
             1
         } else {
             0
@@ -49,10 +53,41 @@ fn count_valid_combinations(row: &str, broken_pattern: &Vec<usize>) -> usize {
     }
 }
 
+/// Thanks to https://www.youtube.com/watch?v=g3Ms5e7Jdqo for explaining
+/// this solution. Nice little logic puzzle. 
+#[memoize]
+fn count_combinations(row: String, pattern: Vec<usize>) -> usize {
+    if row.chars().count() == 0 {
+        // If we have reached the end of the row, 
+        // and we still have numbers to match, then it is not a valid combination.
+        return if pattern.len() == 0 { 1 } else { 0 }
+    }
+    
+    if pattern.len() == 0 {
+        // If after all of the numbers we are left with any non-matched, 
+        // then it is not a valid combination. 
+        return if row.contains("#") { 0 } else { 1 }
+    }
+
+    let mut count = 0;
+    if ".?".contains(row.chars().nth(0).unwrap()) {
+        count += count_combinations(row.chars().skip(1).collect(), pattern.clone());
+    }
+
+    if "#?".contains(row.chars().nth(0).unwrap()) {
+        if pattern[0] <= row.chars().count() && 
+            row.chars().take(pattern[0]).all(|c| c != '.') &&
+            (pattern[0] == row.chars().count() || row.chars().nth(pattern[0]).unwrap() != '#') 
+        {
+            count += count_combinations(row.chars().skip(pattern[0] + 1).collect(), pattern[1..].to_vec());
+        }
+    }
+    count
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use indicatif::ParallelProgressIterator;
     use rayon::prelude::*;
 
 
@@ -63,12 +98,28 @@ mod tests {
         let (broken_map, broken_pattern) = parse_input(INPUT);
         let count: Vec<usize> = broken_map
             .par_iter()
-            .progress_count(broken_map.len() as u64)
             .zip(broken_pattern)
-            .map(|(row, pattern)| count_valid_combinations(row, &pattern))
+            .map(|(row, pattern)| count_combinations(row.to_string(), pattern))
             .collect();
 
         assert_eq!(count.iter().sum::<usize>(), 7939);
+    }
+
+    #[test]
+    fn day12_part2() {
+        let (broken_map, broken_pattern) = parse_input(INPUT);
+        let count: Vec<usize> = broken_map
+            .iter()
+            .zip(broken_pattern)
+            .map(|(row, pattern)| {
+                let joined_row = row.clone() + &("?".to_string() + row).repeat(4);
+                let join_broken_pattern = pattern.repeat(5);
+                count_combinations(joined_row, join_broken_pattern)
+            })
+            .collect();
+
+        assert_eq!(count.iter().sum::<usize>(), 850504257483930);
+
     }
 
     #[test]
