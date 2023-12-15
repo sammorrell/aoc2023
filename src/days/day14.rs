9dd28adf1329b98d14a6a9eb45742190b9e48633
Sam Morrell <samuelmorrell@gmail.com>
2023-12-15 00:12:39 +0000

    [Day 14] Solution for part 2.
    After seeing suggestions, I opted to try and find the loop in the cycles. This way we only have to evolve the grid forward by the mod of the cycle with 1E9.

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -1,4 +1,3 @@
-
 pub fn fall(col: Vec<char>) -> Vec<char> {
     let mut result = col.clone();
     let mut nchange = 1;
@@ -11,6 +10,52 @@ pub fn fall(col: Vec<char>) -> Vec<char> {
                 nchange += 1;
             }
         }
+    }
+    result
+}
+
+pub fn do_cycle(grid: &Vec<Vec<char>>) -> Vec<Vec<char>>{
+    let mut res = grid.clone();
+   for _ in 0..3 {
+        res = aoctk::grid::transpose(res);
+        res = res.iter().map(|col| fall(col.clone())).collect::<Vec<Vec<char>>>();
+        res = res.iter().map(|row| row.iter().rev().cloned().collect()).collect()
+   }
+   res
+}
+
+pub fn grid_hash(grid: &Vec<Vec<char>>) -> String {
+    grid.iter().map(|row| row.iter().collect::<String>()).collect::<Vec<String>>().join("")
+}
+
+pub fn find_cycle_len(grid: &Vec<Vec<char>>) -> (usize, usize) {
+    let mut result = grid.clone();
+    let mut i = 0;
+    let mut seen_states: Vec<String> = Vec::new();
+
+    loop {
+        let new_grid = do_cycle(&result);
+        result = new_grid;
+
+        if seen_states.contains(&grid_hash(&result)) {
+            break;
+        }
+        
+        seen_states.push(grid_hash(&result));
+        i += 1;
+    }
+
+    let offset = seen_states.iter().position(|el| *el == grid_hash(&result)).unwrap();
+    let cycle_len = i - offset;
+    
+    (offset, cycle_len)
+}
+
+pub fn spin_n(grid: &Vec<Vec<char>>, cycles: usize) -> Vec<Vec<char>> {
+    let mut result = grid.clone();
+    for _ in 0..cycles {
+        let new_grid = do_cycle(&result);
+        result = new_grid;
     }
     result
 }
@@ -36,5 +81,23 @@ mod tests {
             .sum::<usize>();
 
         assert_eq!(result, 136);
+    }
+
+    #[test]
+    fn day14_part2() {
+        const TARGET_CYCLES: usize = 1000000000;
+        let grid = aoctk::io::grid_from_file(Path::new("data/day14/input.txt")).expect("Could not read grid");
+        let (offset, cycle_len) = find_cycle_len(&grid);
+        let spun = spin_n(&grid, offset + (TARGET_CYCLES - offset) % cycle_len );
+
+        let result = spun
+            .iter()
+            .enumerate()
+            .map(|(i, row)| {
+                row.iter().filter(|&&c| c == 'O').count() * (spun.iter().count() - i)
+            })
+            .sum::<usize>();
+
+        assert_eq!(result, 105606);
     }
 }
