0811eb95e3bf77479a5cce6666ecec851e165ef1
Sam Morrell <samuelmorrell@gmail.com>
2023-12-17 23:12:01 +0000

    [Day 17] Solution for part 2.
    Relatively easy solution for part 2. Took a few attempts to get the logic correct, but otherwise a relatively nice part 2.

--- src/days/day17.rs
+++ src/days/day17.rs
@@ -93,6 +93,71 @@ mod tests {
             }
         }
 
-        assert_eq!(hl, 102);
+        assert_eq!(hl, 1195);
+    }
+
+    #[test]
+    fn day17_part2() {
+        let grid = aoctk::io::grid_from_file(Path::new("data/day17/input.txt"))
+            .expect("Unable to find grid file.");
+        let grid: Vec<Vec<usize>> = grid
+            .iter()
+            .map(|row| row.iter().map(|val| val.to_string().parse::<usize>().unwrap() ).collect())
+            .collect();
+
+        let target = (grid.len() as i32 - 1 , grid[0].len() as i32 - 1);
+        let mut states = BinaryHeap::from(vec![State{ hl: 0, pos: (0, 0), dir: (0, 0), dir_steps: 0 }]);
+        let mut seen_states = HashSet::new();
+        let mut hl = 0;
+
+        while let Some(curr) = states.pop() {
+            hl = curr.hl;
+
+            if curr.pos == target {
+                break;
+            }
+
+            if seen_states.contains(&curr.summary()) { continue; }
+
+            seen_states.insert(curr.summary());
+
+            if curr.dir != (0, 0) && curr.dir_steps < 10 {
+                let mut next = curr.clone();
+                next.pos.0 = curr.pos.0 + curr.dir.0;
+                next.pos.1 = curr.pos.1 + curr.dir.1;
+                next.dir_steps += 1;
+                if next.pos.0 >= 0 
+                        && next.pos.1 >= 0
+                        && next.pos.0 < grid.len() as i32 
+                        && next.pos.1 < grid[0].len() as i32
+                {
+                    next.hl += grid[next.pos.0 as usize][next.pos.1 as usize];
+                    states.push(next);
+                }
+            }
+
+            if curr.dir_steps >= 4 || curr.dir == (0, 0) {
+                for dir in vec![(1, 0), (0, 1), (-1, 0), (0, -1)] {
+                    if dir != curr.dir && dir != (-curr.dir.0, -curr.dir.1) {
+                        let mut next = curr.clone();
+                        next.dir = dir;
+                        next.pos.0 = next.pos.0 + next.dir.0;
+                        next.pos.1 = next.pos.1 + next.dir.1;
+                        next.dir_steps = 1;
+    
+                        if next.pos.0 >= 0 
+                            && next.pos.1 >= 0
+                            && next.pos.0 < grid.len() as i32 
+                            && next.pos.1 < grid[0].len() as i32
+                            {
+                                next.hl += grid[next.pos.0 as usize][next.pos.1 as usize];
+                                states.push(next);
+                        }
+                    }
+                }
+            }
+        }
+
+        assert_eq!(hl, 1347);
     }
 }
